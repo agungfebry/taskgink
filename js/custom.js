521d2fff12bde0bdcd5b81b869f83391eed8f637
agungfebry <agungfebry97@gmail.com>
2021-03-15 17:09:14 +0700

    new

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,19 @@
 var addTask = document.getElementById("addTask");
 
+var overlay = document.getElementById('overlay');
+overlay.classList.remove('overlay');
+
 addTask.addEventListener('click', () => {
     var slideAddTask = document.getElementById('slideAddTask');
     var close = document.getElementById('close');
 
     slideAddTask.classList.add('slideTop');
     slideAddTask.classList.remove('slideDown');
+    overlay.classList.add('overlay');
 
     close.addEventListener('click', () => {
         slideAddTask.classList.remove('slideTop');
         slideAddTask.classList.add('slideDown');
+        overlay.classList.remove('overlay');
     })
-
 });
